8a865192be1b0546f1c455110d505666e3a06439
Vlad <64969216+dreamkaman@users.noreply.github.com>
2021-11-23 16:55:48 +0200

    next

--- src/index.js
+++ src/index.js
@@ -4,24 +4,55 @@ var debounce = require('lodash.debounce');
 
 import Notiflix from 'notiflix';
 
+import { fetchedCountries } from "./fetchCountries";
+
 const DEBOUNCE_DELAY = 300;
 
 const inputEl = document.querySelector('input[id="search-box"]');
 
-const callback = event => {
+const ulEl = document.querySelector('.country-list');
 
-    console.dir(event.target.value);
+//console.log(ulEl);
+
+const onInputCountryName = event => {
+    
+    let countryName = event.target.value.trim();
+    
+    ulEl.innerHTML = '';
+
+    if (countryName !== '') {
+
+        fetchedCountries(countryName).then(data => {
+
+            if (data.length > 10) {
+
+                Notiflix.Notify.failure('Too many matches found. Please enter a more specific name.');
+
+            } else if (data.length > 1) {
+                console.log(data);
+
+                const markUp = data.map(country => {
+
+                    const { flags: { svg }, name: { official } } = country;
+                    
+                    return `<li>
+                    <img src = "${svg}" alt = "flag" width = "60px">
+                    <p>${official}</p>
+                    </li>`;
+                
+                }).join('');
+
+                ulEl.innerHTML = markUp;
+
+            } else {
+                
+            }
+        }
+        );
+
+
+    };
 };
 
-inputEl.addEventListener('input', debounce(callback, 300));
+inputEl.addEventListener('input', debounce(onInputCountryName, DEBOUNCE_DELAY ));
 
-console.log(fetch('https://restcountries.com/v3.1/name/united?fields=name,capital,population,flags,languages').then(response => {
-    if (!response.ok) {
-        throw new Error(response.status);
-        
-    }
-    return response.json();
-
-}
-)
-);
